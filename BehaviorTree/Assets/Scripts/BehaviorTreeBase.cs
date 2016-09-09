6968d4299ead8f80ce568c6b88821756c0da2036
Emanuel Strömgren <emanuel.stromgren@pulpeten.se>
2016-09-09 13:18:26 +0200

    Expanded testscene with enemy object, and added fleeing behavior to TestAI. Added WIP Behavior Tree visualization - will get reworked into it's own classes.

--- BehaviorTree/Assets/Scripts/BehaviorTreeBase.cs
+++ BehaviorTree/Assets/Scripts/BehaviorTreeBase.cs
@@ -1,11 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public enum BehaviorState {
-    Success,
-    Failure,
-    Running,
-    Error
+public struct BehaviorState {
+    public static readonly byte Success = 0;
+    public static readonly byte Failure = 1;
+    public static readonly byte Running = 2;
+    public static readonly byte Error = 3;
+
+    public byte Value;
+
+    public static BehaviorState operator !(BehaviorState aOther) {
+        if(aOther.Value == Success) {
+            return new BehaviorState(Failure);
+        } else if(aOther.Value == Failure) {
+            return new BehaviorState(Success);
+        }
+
+        return new BehaviorState(aOther);
+    }
+
+    public override bool Equals(object obj) {
+        return base.Equals(obj);
+    }
+    public override int GetHashCode() {
+        return base.GetHashCode();
+    }
+
+    public static bool operator ==(BehaviorState aThis, byte aOther) {
+        return aThis.Value == aOther;
+    }
+    public static bool operator !=(BehaviorState aThis, byte aOther) {
+        return aThis.Value != aOther;
+    }
+    public static implicit operator BehaviorState(byte aOther) {
+        return new BehaviorState(aOther);
+    }
+
+    public BehaviorState(byte aValue) {
+        Value = aValue;
+    }
+    public BehaviorState(BehaviorState aOther) {
+        Value = aOther.Value;
+    }
 }
 
 public class BehaviorTree<T> {
@@ -57,20 +93,57 @@ public class BTBlackboard {
 }
 
 public abstract class BTNode {
-    protected string mName = "Node";
-    public string Name {
-        get { return mName; }
+    public abstract string NodeText {
+        get;
     }
     public override string ToString() {
-        return Name;
+        return NodeText;
     }
     public abstract BehaviorState UpdateNode();
+    
+#if UNITY_EDITOR
+    private Vector2 mNodeSize = new Vector2(160, 40);
+    public Vector2 NodeSize {
+        get { return mNodeSize; }
+    }
+    private Vector2 mChildNodeOffset = new Vector2(10, 20);
+    protected Vector2 ChildNodeOffset {
+        get { return mChildNodeOffset; }
+    }
+    protected virtual Color BoxColor {
+        get { return Color.blue; }
+    }
+    protected virtual Texture BoxTexture {
+        get {
+            Texture2D ReturnTexture = new Texture2D(1, 1);
+
+            ReturnTexture.wrapMode = TextureWrapMode.Repeat;
+            ReturnTexture.SetPixel(0, 0, BoxColor);
+            ReturnTexture.Apply();
+
+            return ReturnTexture;
+        }
+    }
+    /// <summary>
+    /// Get the size of the node.
+    /// </summary>
+    /// <returns>Node size</returns>
+    public virtual Vector2 GetRenderSize() {
+        return NodeSize;
+    }
+    public virtual void RenderNode(Vector2 aFrom) {
+
+        GUI.Box(new Rect(aFrom, NodeSize), NodeText);
+    }
+#endif
 }
 
 // --- Composites ---
 public abstract class BTComposite : BTNode {
     private int mChildIndex = 0;
-    protected new string mName = "Composite";
+    public override string NodeText {
+        get { return "Composite"; }
+    }
     /// <summary>
     /// The current index in the Composite.
     /// </summary>
@@ -96,9 +169,42 @@ public abstract class BTComposite : BTNode {
         }
         ChildNodes.RemoveAt(aIndex);
     }
+
+#if UNITY_EDITOR
+    public override Vector2 GetRenderSize() {
+        Vector2 TotalRenderSize = new Vector2();
+
+        foreach(BTNode ChildNode in ChildNodes) {
+            Vector2 ChildRenderSize = ChildNode.GetRenderSize();
+            TotalRenderSize.x += ChildRenderSize.x + ChildNodeOffset.x;
+            TotalRenderSize.y = Mathf.Max(TotalRenderSize.y, ChildRenderSize.y + NodeSize.y + ChildNodeOffset.y);
+        }
+        TotalRenderSize.x -= ChildNodeOffset.x;
+
+        return new Vector2(Mathf.Max(NodeSize.x, TotalRenderSize.x), Mathf.Max(NodeSize.y, TotalRenderSize.y));
+    }
+    public override void RenderNode(Vector2 aFrom) {
+        GUI.Box(new Rect(aFrom - ChildNodeOffset / 2, GetRenderSize() + ChildNodeOffset), GUIContent.none);
+
+        
+
+        base.RenderNode(new Vector2(GetRenderSize().x / 2 - NodeSize.x / 2, 0) + aFrom);
+        //UnityEditor.EditorGUI.DrawRect(new Rect(aFrom, NodeSize), Color.gray);
+
+        float CoordXAdd = 0;
+
+        foreach(BTNode ChildNode in ChildNodes) {
+            ChildNode.RenderNode(aFrom + new Vector2(CoordXAdd, NodeSize.y + ChildNodeOffset.y));
+
+            CoordXAdd += ChildNode.GetRenderSize().x + ChildNodeOffset.x;
+        }
+    }
+#endif
 }
 public class BTComp_Sequence : BTComposite {
-    protected new string mName = "Composite: Sequence";
+    public override string NodeText {
+        get { return "Composite: Sequence"; }
+    }
     /// <summary>
     /// Iterates through child nodes. 
     /// Break execution on Failure, pauses execution on Running, return Success if all children succeed.
@@ -136,7 +242,9 @@ public class BTComp_Sequence : BTComposite {
     }
 }
 public class BTComp_Selector : BTComposite {
-    protected new string mName = "Composite: Selector";
+    public override string NodeText {
+        get { return "Composite: Selector"; }
+    }
     /// <summary>
     /// Iterates through child nodes.
     /// Breaks exectution on Success, pauses execution on Running, return Failure if all children fail.
@@ -149,6 +257,8 @@ public class BTComp_Selector : BTComposite {
             if(NodeState == BehaviorState.Success) {
                 // Reset Iterator
                 ChildIndex = 0;
+
+                return BehaviorState.Success;
             }
             if(NodeState == BehaviorState.Running) {
                 // Keep Iterator
@@ -173,7 +283,6 @@ public class BTComp_Selector : BTComposite {
 
 // --- Decorators ---
 public abstract class BTDecorator : BTNode {
-    protected new string mName = "Decorator";
     private BTNode mNode = null;
     public BTNode Node {
         get { return mNode; }
@@ -182,9 +291,23 @@ public abstract class BTDecorator : BTNode {
     protected BTDecorator(BTNode aDecoratedNode) {
         mNode = aDecoratedNode;
     }
+#if UNITY_EDITOR
+    public override Vector2 GetRenderSize() {
+        Vector2 ChildNodeSize = Node.GetRenderSize();
+
+        return new Vector2(Mathf.Max(NodeSize.x, ChildNodeSize.x), NodeSize.y + ChildNodeSize.y + ChildNodeOffset.y);
+    }
+    public override void RenderNode(Vector2 aFrom) {
+        base.RenderNode(aFrom);
+
+        Node.RenderNode(aFrom + new Vector2(0, NodeSize.y + ChildNodeOffset.y));
+    }
+#endif
 }
 public class BTDeco_Inverter : BTDecorator {
-    protected new string mName = "Decorator: Inverter";
+    public override string NodeText {
+        get { return "Decorator: Inverter"; }
+    }
 
     public BTDeco_Inverter(BTNode aDecoratedNode) : base(aDecoratedNode) { }
 
@@ -194,21 +317,12 @@ public class BTDeco_Inverter : BTDecorator {
         }
 
         BehaviorState ChildState = Node.UpdateNode();
-
-        if(ChildState == BehaviorState.Running || ChildState == BehaviorState.Error) {
-            return ChildState;
-        }
-
-        if(ChildState == BehaviorState.Success) {
-            return BehaviorState.Failure;
-        }
-        return BehaviorState.Success;
+        return !ChildState;
     }
 }
 
 // --- Leafs ---
 public abstract class BTLeaf<T> : BTNode {
-    protected new string mName = "Leaf";
     private BehaviorTree<T> mBehaviorTree = null;
     protected BTBlackboard Blackboard {
         get { return mBehaviorTree.Blackboard; }
@@ -227,7 +341,9 @@ public abstract class BTCondition<T> : BTLeaf<T> {
     protected BTCondition(BehaviorTree<T> aBehaviorTree) : base(aBehaviorTree) { }
 }
 public class BTCond_InDistance<T> : BTCondition<T> where T : MonoBehaviour {
-    protected new string mName = "Condition: In Distance";
+    public override string NodeText {
+        get { return "Condition: In Distance\n" + TargetName + ", " + Distance.ToString(); }
+    }
 
     private string mTargetName = "";
     public string TargetName {
@@ -264,11 +380,14 @@ public class BTCond_InDistance<T> : BTCondition<T> where T : MonoBehaviour {
     }
 }
 
+
 public abstract class BTTask<T> : BTLeaf<T> {
     protected BTTask(BehaviorTree<T> aBehaviorTree) : base(aBehaviorTree) { }
 }
 public class BTTask_MoveTowards<T> : BTTask<T> where T : MonoBehaviour {
-    protected new string mName = "Task: Move Towards";
+    public override string NodeText {
+        get { return "Task: Move Towards\n" + TargetName + ", " + Speed.ToString(); }
+    }
 
     private string mTargetName = "";
     public string TargetName {
@@ -299,8 +418,9 @@ public class BTTask_MoveTowards<T> : BTTask<T> where T : MonoBehaviour {
         }
 
         Vector3 Offset = Target.transform.position - Agent.transform.position;
-        Agent.transform.position += Offset.normalized * Mathf.Min(Offset.magnitude, Speed);
+        Agent.transform.position += Offset.normalized * (Speed > 0 ? Mathf.Min(Offset.magnitude, Speed) : Speed);
 
         return BehaviorState.Success;
     }
 }
+
--- BehaviorTree/Assets/Scripts/TestAI.cs
+++ BehaviorTree/Assets/Scripts/TestAI.cs
@@ -9,6 +9,12 @@ public class TestAI : MonoBehaviour {
         get { return mTargetObject; }
         set { mTargetObject = value; }
     }
+    [SerializeField]
+    private GameObject mEnemyObject;
+    public GameObject EnemyObject {
+        get { return mEnemyObject; }
+        set { mEnemyObject = value; }
+    }
 
     private BehaviorTree<TestAI> mBehaviorTree;
     public BehaviorTree<TestAI> BehaviorTree {
@@ -18,17 +24,33 @@ public class TestAI : MonoBehaviour {
 
     void Start () {
         BehaviorTree = new BehaviorTree<TestAI>(this);
+        BTComp_Selector RootSelector = new BTComp_Selector();
 
-        BTComp_Sequence Sequence = new BTComp_Sequence();
-        Sequence.AddNode(new BTDeco_Inverter(new BTCond_InDistance<TestAI>(BehaviorTree, "Target", 1)));
-        Sequence.AddNode(new BTTask_MoveTowards<TestAI>(BehaviorTree, "Target", 0.01f));
+        BTComp_Sequence FleeSequence = new BTComp_Sequence();
+        FleeSequence.AddNode(new BTCond_InDistance<TestAI>(BehaviorTree, "Enemy", 15));
+        FleeSequence.AddNode(new BTTask_MoveTowards<TestAI>(BehaviorTree, "Enemy", -0.05f));
 
-        BehaviorTree.Blackboard.Objects["Target"] = mTargetObject;
+        BTComp_Sequence TargetSequence = new BTComp_Sequence();
+        TargetSequence.AddNode(new BTDeco_Inverter(new BTCond_InDistance<TestAI>(BehaviorTree, "Target", 1)));
+        TargetSequence.AddNode(new BTTask_MoveTowards<TestAI>(BehaviorTree, "Target", 0.01f));
 
-        BehaviorTree.RootNode = Sequence;
+        RootSelector.AddNode(FleeSequence);
+        RootSelector.AddNode(TargetSequence);
+
+        BehaviorTree.Blackboard.Objects["Target"] = TargetObject;
+        BehaviorTree.Blackboard.Objects["Enemy"] = EnemyObject;
+
+        BehaviorTree.RootNode = RootSelector;
     }
 	
 	void Update () {
         BehaviorTree.UpdateTree();
 	}
+    void OnGUI() {
+        BehaviorTree.RootNode.RenderNode(new Vector2(50, 50));
+    }
+
+    void OnDrawGizmos() {
+        //BehaviorTree.RootNode.RenderNode(new Vector2(100, 300));
+    }
 }
